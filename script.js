81af43ea7e8641ab688f74ea134f2acae5a6e681
harold-sh-23 <harolds808@hotmail.com>
2026-05-04 01:47:09 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector("form").addEventListener("submit", function(){
     alert("Thank you! Your message has been sent. Please allow 24-48 hours for a response.");
 });
+
+function showMessage(){
+    alert("Get 10% off your first custom order!");
+}
