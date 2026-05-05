3e3bb796ba8836afa0f0325bcbf9aa330df44251
harold-sh-23 <harolds808@hotmail.com>
2026-05-04 21:49:21 -1000

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,3 @@ document.addEventListener("DOMContentLoaded", function () {
             alert("Thank you! Your message has been sent. Please allow 24-48 hours for a response.");
         });
     }
-
-});
-
-function showMessage(){
-    alert("Get 10% off your first custom order!");
-}
